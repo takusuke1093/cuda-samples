d220f933bd74045ed60cd85f03c7b16663dc39f8
Takumi Yoshida <takumi.yoshida.seattle@gmail.com>
2020-12-11 16:10:35 +0900

    mesure parallel caluculation with nvprof

--- 256threads_main.cu
+++ 256threads_main.cu
@@ -7,37 +7,38 @@
 #define PI 3.14159265358979323846
 #define DEG_TO_RAD(deg)  ((deg) / 180.0 * (PI))
 
-__global__ void sfu_sine_function(double *B_d, double *radius_d)
+__global__ void normal_cosine_function1_256(double *B_d, double *radius_d)
 {
-	for (int i = 0; i<=N; i+=1) {
-		B_d[i] = __sinf(radius_d[i]);
-	}
+	int t = threadIdx.x;
+	int T = blockDim.x;
+	for (int i = t; i <= N; i += T)
+		B_d[i] = cos(radius_d[i]);
 }
 
 
 int main()
 {
 	int i;
-	double B[N];   // HOST
-	double radius[N];  // HOST
-	double *B_d;   // DEVICE
-	double *radius_d;  // DEVICE
+	double B[N];      // HOST
+	double radius[N];    // HOST
+	double *B_d;      // DEVICE
+	double *radius_d; // DEVICE
 	double deg = 0.0;
 	FILE *outputfile;
 
-	outputfile = fopen("output_sine.txt", "w"); 
+	outputfile = fopen("output_cosine_256threads.txt", "w"); 
 	if (outputfile == NULL) {
 		printf("cannot open file! \n");
 		exit(1);
 	}
 
-	for (i = 0; i <= N; i++) {
+	for (int i = 0; i <= N; i+=1) {
 		radius[i] = DEG_TO_RAD(deg);
 		deg += 0.1;
 	}
 
         dim3 blocks(1,1,1);
-        dim3 threads(1,1,1);
+        dim3 threads(360,1,1);
 
 	cudaMalloc( (void**) &B_d, N*sizeof(double));
 	cudaMalloc( (void**) &radius_d, N*sizeof(double));
@@ -45,7 +46,7 @@ int main()
 	cudaMemcpy(B_d, B, N*sizeof(double), cudaMemcpyHostToDevice); 
 	cudaMemcpy(radius_d, radius, N*sizeof(double), cudaMemcpyHostToDevice); 
 	
-	sfu_sine_function<<< blocks, threads >>>(B_d, radius_d);
+	normal_cosine_function1_256<<< blocks, threads >>>(B_d, radius_d);
 
         cudaMemcpy(B, B_d, N*sizeof(double), cudaMemcpyDeviceToHost);
 	
@@ -60,4 +61,3 @@ int main()
 
     return 0;
 }
-
